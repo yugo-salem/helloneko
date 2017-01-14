f281652086060b440e4884cacbe2b05362eb74ae
yugo <yugosalem@protonmail.com>
2017-01-14 23:16:26 +0300

    styles

--- hello.py
+++ hello.py
@@ -4,13 +4,17 @@
 
 BLINK = '\x1b[5m'
 BOLD = '\x1b[1m'
+BG_DARK = '\x1b[100m'
+WHITE = '\x1b[97m'
 MAGENTA = '\x1b[35m'
-NORMAL = '\x1b[0m\x1b[39m'
+NORMAL = '\x1b[0m\x1b[39m\x1b[49m'
 NAME = 'nekochan777'
 
 
 def main():
-    print(''.join(['Hello', ' ', BLINK, BOLD, MAGENTA, NAME, NORMAL]))
+    print(''.join([BG_DARK, BOLD, WHITE,
+                   'Hello', ' ', BLINK,
+                   MAGENTA, NAME, NORMAL]))
 
 
 if __name__ == '__main__':
