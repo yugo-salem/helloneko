c7d64bb5d2e1aa4ffa12607e6fc9f042374e653a
Neko Chan <cutenekochan777@gmail.com>
2017-03-04 18:57:20 +0300

    вариант с фором

--- test1.py
+++ test1.py
@@ -7,10 +7,14 @@ str="teststring123"
 str2="";
 strlen=len(str)
 print(strlen)
-i=0;
-while i<strlen:
+
+#i=0;
+#while i<strlen:
+#    str2=str2+str[strlen-1-i]
+#    i=i+1
+
+for i in range(strlen):
     str2=str2+str[strlen-1-i]
-    i=i+1
 
 print(str)
 print(str2)
